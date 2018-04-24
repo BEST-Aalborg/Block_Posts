d90af7aa287b6b88b60b7dedb2ab4c1e539e98a6
nn <nn@nn.nn>
2018-04-24 16:24:48 +0200

    rewrote the function find_channel_id_and_name because new function in the slack api has become available

--- src/misc.rs
+++ src/misc.rs
@@ -2,7 +2,7 @@ extern crate serde;
 extern crate serde_json;
 
 use template::api::MessageStandard;
-use template::api::{chat,im,groups,channels};
+use template::api::{chat,im,groups,channels,conversations};
 use template::api::requests;
 use template::api::requests::Client;
 
@@ -21,11 +21,16 @@ pub fn delete_post_from_channel(message: &MessageStandard, slack: &Slack) {
     if message.thread_ts.is_none() {
         let (channel_id, channel_name) = find_channel_id_and_name(message, slack, &client);
 
-        info!("channel_id: {:?} - channel_name: {:?}", &channel_id, &channel_name);
+        if channel_id.is_empty() && channel_name.is_empty() {
+            error!("{:?}", &message);
+        } else {
+            info!("channel_id: {:?} - channel_name: {:?}", &channel_id, &channel_name);
 
-        if CONFIG.channels.as_ref().unwrap().get(&channel_name).is_some() {
-            delete_post(message, &channel_id, &channel_name, slack, &client);
+            if CONFIG.channels.as_ref().unwrap().get(&format!("#{}", &channel_name)).is_some() {
+                delete_post(message, &channel_id, &channel_name, slack, &client);
+            }
         }
+
     }
 }
 
@@ -36,32 +41,21 @@ fn find_channel_id_and_name(message: &MessageStandard, slack: &Slack, client: &C
     let mut channel_id = String::new();
     let mut channel_name = String::new();
 
-    // Check of the message ID is for a channel
-    let result = channels::info(client, &slack.api_token, &channels::InfoRequest {
+    // Gets if about the channel
+    let result = conversations::info(client, &slack.api_token, &conversations::InfoRequest {
         channel: &message.channel.as_ref().unwrap(),
+        include_locale: None,
     });
-    match result { // if the ID belongs to a channel, the channel_id and channel_name will be returned
+
+    match result { // if the ID belongs to a channel or a group, the channel_name will be set
         Ok(c) => {
-            channel_id = c.channel.as_ref().unwrap().id.as_ref().unwrap().clone();
-            channel_name = c.channel.as_ref().unwrap().name.as_ref().unwrap().clone();
-            return (channel_id, format!("#{}", channel_name));
+            channel_id = c.channel.as_ref().unwrap().id.as_ref().unwrap_or(&String::new()).clone();
+            channel_name = c.channel.as_ref().unwrap().name.as_ref().unwrap_or(&String::new()).clone();
         },
         Err(_) => (),
     }
 
-    // Check of the message ID is for group
-    let result = groups::info(client, &slack.api_token, &groups::InfoRequest {
-        channel: &message.channel.as_ref().unwrap(),
-    });
-    match result { // if the ID belongs to a group, the channel_id and group_name will be return
-        Ok(c) => {
-            channel_id = c.group.as_ref().unwrap().id.as_ref().unwrap().clone();
-            channel_name = c.group.as_ref().unwrap().name.as_ref().unwrap().clone();
-        },
-        Err(_) => (),
-    }
-
-    (channel_id, format!("#{}", channel_name))
+    (channel_id, channel_name)
 }
 
 /// Deletes the message and sends a response to the user that his message has been deleted.
@@ -86,6 +80,8 @@ fn delete_post(message: &MessageStandard, channel_id: &String, channel_name: &St
 
     let result = chat::post_message(client, &slack.api_token, &chat::PostMessageRequest {
         channel: instant_message.id.as_ref().unwrap(),
+        // How to link to a channel: The syntax is "<#CHANNEL_ID|CHANNEL_NAME>"
+        // Note: There is no hashtag in front of the CHANNEL_NAME
         text: &format!("You are not allowed to create a post this channel <#{}|{}>, please use the thread function. I have attached the message you was trying to post, Stay PANDA!",
                        &channel_id,
                        &channel_name),
